8d07be540e6da00eef59bd95c85eb7d1fb82efec
jjjpanda <jawpanda13@gmail.com>
2020-01-04 17:38:06 -0500

    update earnings as command; message history

--- db/earningsDaemon.js
+++ db/earningsDaemon.js
@@ -5,58 +5,61 @@ const Earnings = require('./models/Earnings');
 
 const daemon = cron.schedule(  //'*/30 * * * * *',
   '0 0-11 * * *',
-  () => {
-    console.log('---DB REQUESTED CRON JOB---');
-    console.log('TITLE: UPDATE EARNINGS');
-    let date = new Date();
-    const days = 6;
-    let iterations = 0;
-
-    const loop = setInterval(() => {
-      processing.getERCalendar(date, (company, d) => {
-        Earnings.findOne({ company }).then((earnings) => {
-          if (earnings) {
-            if (d.getFullYear() == earnings.date.getFullYear()
-              && d.getMonth() == earnings.date.getMonth()
-              && d.getDate() == earnings.date.getDate()) {
-
-            } else {
-              earnings.date = new Date(d);
-              earnings.save();
-            }
-          } else {
-            const NewEarnings = new Earnings({
-              date: new Date(d),
-              company,
-            });
-            NewEarnings.save();
-          }
-        });
-      });
-      iterate();
-    }, 1500);
-    var iterate = () => {
-      date = new Date(date.setDate(date.getDate() + 1));
-      iterations++;
-      if (iterations >= days) {
-        clearInterval(loop);
-      }
-    };
-
-    Earnings.deleteMany({ date: { $lt: new Date() } })
-      .then((e) => {
-        if (!e.ok) {
-          console.log('UPDATE EARNINGS: Previous Dates Removed');
-        }
-      });
-  }, {
+  daemon, {
     scheduled: false,
     timezone: 'America/New_York',
   },
 );
 
+const daemonFunction = () => {
+  console.log('---DB REQUESTED CRON JOB---');
+  console.log('TITLE: UPDATE EARNINGS');
+  let date = new Date();
+  const days = 6;
+  let iterations = 0;
+
+  const loop = setInterval(() => {
+    processing.getERCalendar(date, (company, d) => {
+      Earnings.findOne({ company }).then((earnings) => {
+        if (earnings) {
+          if (d.getFullYear() == earnings.date.getFullYear()
+            && d.getMonth() == earnings.date.getMonth()
+            && d.getDate() == earnings.date.getDate()) {
+
+          } else {
+            earnings.date = new Date(d);
+            earnings.save();
+          }
+        } else {
+          const NewEarnings = new Earnings({
+            date: new Date(d),
+            company,
+          });
+          NewEarnings.save();
+        }
+      });
+    });
+    iterate();
+  }, 1500);
+  var iterate = () => {
+    date = new Date(date.setDate(date.getDate() + 1));
+    iterations++;
+    if (iterations >= days) {
+      clearInterval(loop);
+    }
+  };
+
+  Earnings.deleteMany({ date: { $lt: new Date() } })
+    .then((e) => {
+      if (!e.ok) {
+        console.log('UPDATE EARNINGS: Previous Dates Removed');
+      }
+    });
+}
+
 module.exports = {
   start: daemon.start,
   stop: daemon.stop,
   destroy: daemon.destroy,
+  daemon: daemonFunction
 };
