e53bef06637e3da654a44f32818c5343f3d393b1
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-04-28 16:55:04 -0400

    updates

--- db/appendLogs.js
+++ db/appendLogs.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
-const path = require('path')
 module.exports = (filePath, str) => {
-    fs.appendFile(path.resolve(__dirname, filePath), str+'\n', (err) => {
+    fs.appendFile(filePath, str+'\n', (err) => {
         if (err) {
             console.log("Append to " + filePath + " Failed")
         } else {
